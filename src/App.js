07472d589bfb4742088dfe330a72bb5ee6bea1ba
NAOYA804 <9494suruna@gmail.com>
2025-06-21 08:16:58 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2em' }}>
+      <h1>🎵 Utariku Music App 🎵</h1>
+      <p>ここにあなたの楽曲リストアプリを追加しましょう！</p>
+    </div>
+  );
+}
+
+export default App;
